51c77ecd55b771de70172a135ebbe9a97e4ac889
Philipp Block <philipp.block@fintu.ai>
2019-07-18 18:27:43 +0200

    Add engine to `to_parquet` to be explicit

--- test_fastparquet.py
+++ test_fastparquet.py
@@ -3,5 +3,5 @@ import pandas as pd
 print("Creating dataframe")
 df = pd.DataFrame([[1,2],[3,4]], columns=["c1", "c2"])
 print("Saving dataframe")
-df.to_parquet("test.pqt", compression="uncompressed")
+df.to_parquet("test.pqt", engine="fastparquet", compression="uncompressed")
 print("Done")
